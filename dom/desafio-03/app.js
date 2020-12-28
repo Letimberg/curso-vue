9a759f0f98adedb201c56ccc93c59656330d5d9b
Letimberg Cavalcante <letimberg.cavalcante@hemoce.ce.gov.br>
2020-12-27 22:42:33 -0300

    [update][frontend] #3 - Estilo Dinâmico e Clases CSS #2

--- dom/desafio-03/app.js
+++ dom/desafio-03/app.js
@@ -2,5 +2,27 @@ new Vue({
     el: '#desafio',
     data: {
         valor: 0
+    },
+
+    computed: {
+        resultado() {
+            return this.valor == 37 ? 'Valor igual 37' : 'Valor diferente 37'
+        }
+    },
+
+    watch: {
+        resultado() {
+            setTimeout(() => {
+                this.valor = 0
+            }, 5000)
+        }
+    },
+    methods: {
+        aumentar() {
+            this.valor++
+        },
+        diminuir() {
+            this.valor--
+        }
     }
 });
